2259f937f2ba1abc70cb0408cd6b4021164673af
maltn <37661991+maltn@users.noreply.github.com>
2022-03-01 13:46:54 +0000

    Deploying to pages from @ maltn/lostark-mari@fc9410471d708b623dfc2305e688b104b811bd0d 🚀

---
+++ _next/static/O1simdf3EIOpVgFd5H4wK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fe8c8a1df91eab43.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
